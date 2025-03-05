9342a20d382376ef77d010f36d3282542e254536
anangben <anangben932@gmail.com>
2025-03-05 15:04:33 +0000

    first commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,28 @@
-export const addProduct = (req, res) => {
-  //Check if user has permission
-  // Upload product image
-  //Validate product information
-  //Save product information in database
-  //return response
-  res.json(req.body);
+import { ProductModel } from "../models/product.js";
+
+export const addProduct = async (req, res, next) => {
+  try {
+    //Check if user has permission
+    // Upload product image
+    //Validate product information
+    //Save product information in database
+    const result = await ProductModel.create(req.body);
+    //return response
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getProducts = (req, res) => {
-  res.send("All products!");
+export const getProducts = async (req, res) => {
+  try {
+    //Fetch products from database
+    const result = await ProductModel.find();
+    //Return response
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const countProducts = (req, res) => {
