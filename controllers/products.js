d2fd5489ff16577922140c8583314a6dbe34f824
anangben <anangben932@gmail.com>
2025-03-10 11:20:38 +0000

    validation implemented

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,21 @@
 import { ProductModel } from "../models/product.js";
+import { addProductValidator } from "../validators/products.js";
 
 export const addProduct = async (req, res, next) => {
   try {
     //Check if user has permission
     // Upload product image
     //Validate product information
+    const { error, value } = addProductValidator.validate(req.body, {
+      abortEarly: false,
+    });
+    if (error) {
+      return res.status(422).json(error);
+    }
     //Save product information in database
-    const result = await ProductModel.create(req.body);
+    const result = await ProductModel.create(value);
     //return response
-    res.json(result);
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
